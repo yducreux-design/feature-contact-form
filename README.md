1da7b879fabce765b6734a627a89dba709420af0
yducreux-design <yotis.duc@gmail.com>
2022-05-09 14:40:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/L2VE/workshop.git
+git push -u origin main
---
+++ index.php
@@ -0,0 +1,19 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post" action="handler.php">
+            <label for="input-username">username:</label>
+            <input type="text" name="data-username" id="input-username">
+            <label for="input-age">age:</label>
+            <input type="text" name="data-age" id="input-age">
+            <input type="submit" name="btn-submit">;
+        </form>
+    
+    </body>
+    </html>
