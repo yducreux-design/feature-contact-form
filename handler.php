53ec7dddf115f7bc9a9e201f081e43c0dd20d686
yducreux-design <yotis.duc@gmail.com>
2022-05-09 15:21:14 +0200

    structure contact form

--- handler.php
+++ handler.php
@@ -1,19 +1,10 @@
 <?php
 
     $username = $_POST['data-username'];
-    $age = $_POST['data-age'];
+    $mail = $_POST['data-mail'];
+    $subject = $_POST['data-subject'];
+    $message = $_POST['data-message'];
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Salut, <?php echo $username;?>, tu as <?php echo $age;?> ans !</p>
-</body>
-</html>
+
+
--- index.php
+++ index.php
@@ -9,10 +9,14 @@
     <body>
         <form method="post" action="handler.php">
             <label for="input-username">username:</label>
-            <input type="text" name="data-username" id="input-username">
-            <label for="input-age">age:</label>
-            <input type="text" name="data-age" id="input-age">
-            <input type="submit" name="btn-submit">;
+            <input type="text" name="data-username" id="input-username"> <br>
+            <label for="input-mail">mail:</label>
+            <input type="email" name="data-mail" id="input-mail"> <br>
+            <label for="input-subject">subject:</label>
+            <input type="text" name="data-subject" id="input-subject"> <br>
+            <label for="input-message">message</label>
+            <textarea name="data-message" id="input-message" cols="30" rows="10"></textarea> <br>   
+            <input type="submit" name="btn-submit"> 
         </form>
     
     </body>
