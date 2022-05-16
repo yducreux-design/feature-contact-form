7e5edb32caab44f3bd2643e5729058ffdaf493ca
yducreux-design <yotis.duc@gmail.com>
2022-05-16 10:13:54 +0200

    finish

--- showHide.js
+++ showHide.js
@@ -1,7 +1,7 @@
 let hideField = document.getElementById("hideField");
 let showPassword = document.getElementById("showPassword");
 
-showPassword.onmousedown = function(){
+/*showPassword.onmousedown = function(){
 
     hideField.type = "text";
     showPassword.innerText ="Relacher pour cacher";
@@ -11,3 +11,24 @@ showPassword.onmouseup = function(){
     hideField.type = "password";
     showPassword.innerText ="Maintenir pour afficher";
 }
+
+//cacher si click and drag
+    showPassword.ondrag = function(){
+
+        hideField.type = "password";
+        showPassword.innerText ="Maintenir pour afficher";
+    } */
+
+    let btnshow = false;
+
+    showPassword.onclick = function(){
+        btnshow = !btnshow;
+        if (btnshow) {
+            hideField.type = "text";
+            showPassword.innerText ="Cliquer pour cacher";
+        } else{
+            
+        hideField.type = "password";
+        showPassword.innerText ="Cliquer pour afficher";
+        }
+    }
